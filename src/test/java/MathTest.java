bdebc80b8188a4196f6ab021340a3282c6cf5aa4
Sunshinezmh <18333640045@163.com>
2019-10-19 21:53:30 +0800

    lambda表达式实现的加减乘除--张明慧--2019年10月19日21:53:25

---
+++ src/test/java/MathTest.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @Classname MathTest
+ * @Auther sunshinezhang
+ * @Date 2019/10/19 21:06
+ */
+public class MathTest {
+
+	public static void main(String[] args) {
+		MathOperatioin add = ((a, b) -> a + b);
+		Assert.assertEquals(9,LambdaBasic.operation(4, 5, add));
+
+
+		MathOperatioin differ = ((a, b) -> a - b);
+		Assert.assertEquals(4, LambdaBasic.operation(11, 7, differ));
+
+		MathOperatioin multiple = ((a, b) -> a * b);
+		Assert.assertEquals(18, LambdaBasic.operation(3, 6, multiple));
+
+		MathOperatioin division = ((a, b) -> a / b);
+		Assert.assertEquals(5,LambdaBasic.operation(15,3,division));
+
+	}
+
+}
